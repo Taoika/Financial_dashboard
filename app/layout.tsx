570c07b3912b3344f297bc00e5126f0ff7abcb99
taoika <670335957@qq.com>
2024-12-20 11:07:10 +0800

    换张照片

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="twitter:card" content="player" /> 
-      <meta name="twitter:site" content="@nytimesbits" /> 
-      <meta name="twitter:creator" content="@nickbilton" /> 
+      <meta name="twitter:site" content="@Taoika" /> 
+      <meta name="twitter:creator" content="@Taoika" /> 
 
       <meta property="og:type" content="video"/>
       <meta property="og:title" content="#子猫の成長日記 | 黒猫 | TikTok"/>
       <meta property="og:description" content="#子猫の成長日記 | 黒猫 | TikTok"/>
-      <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"/>
+      <meta property="og:image" content="https://i.pinimg.com/736x/39/b7/65/39b7654014a0c09c8177d09dc47f6100.jpg"/>
 
       <meta property="og:video" content="https://www.tiktok.com/@yiwi88/video/7449309699092188434"/>
       <meta property="og:url" content="https://www.tiktok.com/@yiwi88/video/7449309699092188434"/>
